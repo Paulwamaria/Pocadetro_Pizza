88e04862f5329d2bc7551d9920e34cd6b35356a4
Paulwamaria <paulwamaria@gmail.com>
2019-07-28 13:41:30 +0300

    Add a function to calculate the sum cost of all the selected toppings

--- js/poca.js
+++ js/poca.js
@@ -16,17 +16,30 @@ $("form#pizza-form").submit(function(event) {
   var selectedCrust=$("input[name='crust']:checked").val();
   //Access the selected toppings and put them in an array
   var toppings=[];
-  $.each($("input[name='toppings']:checked"), function(){            
-    toppings.push($(this).val());
+  var totalToppingCost=0;
+  $("input[name='toppings']:checked").each(function(){            
+    toppings.push(parseInt(($(this).val())));
+   
+    // toppings.forEach(topping=> totalToppingCost =totalToppingCost+topping);
+    totalToppingCost=toppings.reduce((totalToppingCost,topping)=>totalToppingCost+topping);
+
+
 });
   var delivaryBool=$("input[name='delivary']:checked").val();
   var numberOfPizzas=$("input#how-many").val();
   var locationName=$("input#locationName").val();
   
 
-  $("ul#totalCost").append("<li><span class='contact'>" + locationName + " hi"+ "</span></li>");
+  $("ul#totalCost").append("<li><span class='contact'>" + totalToppingCost+ " "+ "</span></li>");
 
-
+  //create constructors
+  //back-end
+  // function pizzaPriceCalc(costBySize,costOfCrust,costOfToppings,costOfDelivary,numberOfPizzas){
+  //   this.sizeOfPizza:costBySize,
+  //   this.selectedCrust:costOfCrust,
+  //   this.
+  // }
+  
 });
 
 }
