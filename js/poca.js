45a3c862ee575dd9056c15d3b4fc74245db75b2c
Paulwamaria <paulwamaria@gmail.com>
2019-07-28 11:24:07 +0300

    Access the selected pizza size using jQuery

--- js/poca.js
+++ js/poca.js
@@ -5,6 +5,21 @@ var costOfCrust=0;
 var costOfToppings=0;
 var costOfDelivary=0;
 var numberOfPizzas;
+
+//front-end
+
+
+$("form#pizza-form").submit(function(event) {
+  event.preventDefault();
+
+  var sizeOfPizza = $("#dropdown").children("option").filter(":selected").text()
+  
+
+  // $("ul#totalCost").append("<li><span class='contact'>" + sizeOfPizza + "hi"+ "</span></li>");
+
+
+});
+
 }
 
 );
