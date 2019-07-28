0058f1a5356407182692e8ee983f536e8dfeb591
Paulwamaria <paulwamaria@gmail.com>
2019-07-28 21:03:31 +0300

    Display the user's input plus the total cost on submit

--- js/poca.js
+++ js/poca.js
@@ -6,6 +6,7 @@ var costOfDelivary=250;
 var numberOfPizzas;
 var newPrice;
 var totalCost=0;
+var selectedCrustName;
 
   //create constructors
   //back-end
@@ -29,18 +30,39 @@ $(document).ready(function(){
 $("form#pizza-form").submit(function(event) {
   event.preventDefault();
 
-  var inputedSizeOfPizza =parseInt($("#dropdown").children("option").filter(":selected").val())
+  var inputedSizeOfPizza =parseInt($("#dropdown").children("option").filter(":selected").val());
+  var inputedSizeName=$("#dropdown").children("option").filter(":selected").text();
   var userSelectedCrust=parseInt($("input[name='crust']:checked").val());
+  //get the name of the selected radioButton
+  $("input[name='crust']:checked").each(function(){
+    var idVal = $(this).attr("id");
+     selectedCrustName=$("label[for='"+idVal+"']").text()
+  })
+ 
+
   //Access the selected toppings and put them in an array
   var toppings=[];
+  var toppingNames=[];
   var userTotalToppingCost=0;
   $("input[name='toppings']:checked").each(function(){            
     toppings.push(parseInt(($(this).val())));
    
     // toppings.forEach(topping=> totalToppingCost =totalToppingCost+topping);
     userTotalToppingCost=toppings.reduce((userTotalToppingCost,topping)=>userTotalToppingCost+topping);
+
+   
     
 });
+
+ //Get the tooping names
+ $("input[name='toppings']:checked").each(function(){
+  var idValue = $(this).attr("id");
+  toppingNames.push($("label[for='"+idValue+"']").text() +" topping @ "+ parseInt(($(this).val())) );
+ });
+//  toppingNames.forEach(function(toppingName){
+//   $("ul#totalCost").append("<li><span class='contact'>" +"You selected " +toppingNames + "ksh"+"</span></li>");
+//  });
+
   var delivaryBool=$("input[name='delivary']:checked").val();
   var inputedNumberOfPizzas=parseInt($("input#how-many").val());
   var locationName=$("input#locationName").val();
@@ -75,9 +97,14 @@ $("form#pizza-form").submit(function(event) {
      fullCost();
 
 
+     //display output
+     $(".output").show();
 
-  $("ul#totalCost").append("<li><span class='contact'>" + totalCost+ " "+ "ksh"+"</span></li>");
-  console.log(totalCost)
+     $("ul#totalCost").append("<li><span class='contact'>" +"You selected " +toppingNames + "ksh"+"</span></li>");  
+  $("ul#totalCost").append("<li><span class='contact'>" +"You selected " +inputedSizeName + " pizza @ "+inputedSizeOfPizza+ "ksh"+"</span></li>");
+  $("ul#totalCost").append("<li><span class='contact'>" +"You selected " +selectedCrustName + " pizza @ "+userSelectedCrust+ "ksh"+"</span></li>");
+  $("ul#totalCost").append("<li><span class='contact'>" +"Total cost is: "+ totalCost+ " "+ "ksh"+"</span></li>");
+ 
   
 });
 
